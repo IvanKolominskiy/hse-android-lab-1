8a30b8d649fc7afc496138ee3b47fb7a87852626
Ivan Kolominsky <ifkolominskiy@edu.hse.ru>
2023-10-24 00:36:14 +0300

    Second activity has been added

---
+++ app/src/main/java/com/example/hse_android_lab_1/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.hse_android_lab_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
