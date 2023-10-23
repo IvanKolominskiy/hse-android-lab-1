e8b1504fc00048161a3bb195fe49f1ec102e2282
Ivan Kolominsky <ifkolominskiy@edu.hse.ru>
2023-10-24 01:03:52 +0300

    Added setting the background color of the second activity

--- app/src/main/java/com/example/hse_android_lab_1/MainActivity.java
+++ app/src/main/java/com/example/hse_android_lab_1/MainActivity.java
@@ -1,12 +1,37 @@
 package com.example.hse_android_lab_1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    EditText editText;
+
+    Button acceptButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        editText = findViewById(R.id.edit_text);
+        acceptButton = findViewById(R.id.accept_button);
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent departingIntent = new Intent(MainActivity.this, SecondActivity.class);
+
+                departingIntent.putExtra("color", editText.getText().toString());
+
+                startActivity(departingIntent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/hse_android_lab_1/SecondActivity.java
+++ app/src/main/java/com/example/hse_android_lab_1/SecondActivity.java
@@ -1,12 +1,37 @@
 package com.example.hse_android_lab_1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class SecondActivity extends Activity {
+
+    RelativeLayout relativeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        Intent recievedIntent = getIntent();
+
+        String color = recievedIntent.getStringExtra("color");
+        relativeLayout = findViewById(R.id.second_activity_relative_layout);
+
+        switch (color) {
+            case "green":
+                relativeLayout.setBackgroundResource(R.color.green);
+                break;
+
+            case "red":
+                relativeLayout.setBackgroundResource(R.color.red);
+                break;
+
+            case "blue":
+                relativeLayout.setBackgroundResource(R.color.blue);
+                break;
+        }
+
     }
 }
