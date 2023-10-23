f8adec500094c93e0fb3aba64e08fc9da6efe853
Ivan Kolominsky <ifkolominskiy@edu.hse.ru>
2023-10-23 21:57:24 +0300

    Main activity has been added

---
+++ app/src/main/java/com/example/hse_android_lab_1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.hse_android_lab_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
